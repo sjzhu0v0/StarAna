8764b65c108a4892e7f9c62d38ec59b39ea40f7a
sjzhu0v0 <zhusenjie@mail.ustc.edu.cn>
2022-08-22 11:00:21 +0800

    QA codes have been modified to match pAu200_15_VPDMB30.

--- QA/StRoot/StMyMaker/StMyCuts.h
+++ QA/StRoot/StMyMaker/StMyCuts.h
@@ -1,24 +1,33 @@
 #ifndef StMyCuts_h
 #define StMyCuts_h
 
-#include "Rtypes.h"
 #include <map>
 #include <vector>
 
-namespace StMyCuts{
-	const std::vector<UInt_t> Trigger   = {720007};
-	const std::vector<Double_t> VzCut   = {198., 202.};
-	const Double_t VrCut                = 2.;
-	const std::vector<Double_t> VrCen   = {0., -2.};
-	const Double_t DcaCut               = 3.;
-	const Int_t    NHitsFitCut          = 10;
-	const Double_t NHitsRatioCut        = 0.52;
-	const Int_t    NHitsDedxCut         = 5;
-	const Double_t NSigmaCut            = 2.;
-	const std::vector<Double_t> PtCutQ  = {0.2, 2.};
-	const std::vector<Double_t> EtaCutQ = {-2., -1.1, -1., 0.};
+#include "Rtypes.h"
 
-	const std::map<Int_t, Int_t> RunIdIndex = {{20355020, 0}, {20355021, 1}, {21044023, 2}, {21044024, 3}, {21044025, 4}, {21044027, 5}, {21044028, 6}, {21044029, 7}, {21044030, 8}, {21044031, 9}, {21044032, 10}, {21044033, 11}, {21044034, 12}, {21044036, 13}, {21044037, 14}, {21044038, 15}, {21044039, 16}, {21044040, 17}, {21044041, 18}, {21044042, 19}, {21044043, 20}, {21045001, 21}, {21045002, 22}, {21045003, 23}, {21045005, 24}, {21045006, 25}, {21045007, 26}, {21045008, 27}, {21045009, 28}, {21045010, 29}, {21045011, 30}};
-}
+namespace StMyCuts {
+const std::vector<UInt_t> Trigger = {500904};
+const std::vector<Double_t> VzCut = {-30., 30.};
+const Double_t VrCut = 2.;
+const std::vector<Double_t> VrCen = {0., -2.};
+const Double_t DcaCut = 3.;
+const Int_t NHitsFitCut = 10;
+const Double_t NHitsRatioCut = 0.52;
+const Int_t NHitsDedxCut = 5;
+const Double_t NSigmaCut = 2.;
+const std::vector<Double_t> PtCutQ = {0.2, 2.};
+const std::vector<Double_t> EtaCutQ = {-2., -1.1, -1., 0.};
+
+const std::map<Int_t, Int_t> RunIdIndex = {
+    {16124017, 0},  {16124018, 1},  {16124019, 2},  {16124033, 3},
+    {16124034, 4},  {16125003, 5},  {16125014, 6},  {16125015, 7},
+    {16125016, 8},  {16125024, 9},  {16125035, 10}, {16125038, 11},
+    {16125039, 12}, {16125042, 13}, {16125043, 14}, {16125046, 15},
+    {16125050, 16}, {16125052, 17}, {16125053, 18}, {16125054, 19},
+    {16125056, 20}, {16125057, 21}, {16125058, 22}, {16125060, 23},
+    {16125061, 24}, {16125062, 25}, {16125063, 26}, {16126006, 27},
+    {16126007, 28}, {16126008, 29}, {16126009, 30}};
+}  // namespace StMyCuts
 
 #endif
--- getfilelist.sh
+++ getfilelist.sh
@@ -1,1 +1,18 @@
-get_file_list.pl -keys path,filename -cond filename~st_physics,trgsetupname=production_pAu200_2015,production=P16id,filetype=daq_reco_picoDst,storage!=HPSS -limit 0 -delim / -distinct > file.list
+if [ $# -lt 1 ]; then
+        get_file_list.pl -keys path,filename -cond filename~st_physics,trgsetupname=production_pAu200_2015,production=P16id,filetype=daq_reco_picoDst,storage!=HPSS -limit 0 -delim / -distinct > file.list
+else
+        if [ ! -f file.list ]; then
+                ./getfilelist.sh
+        fi
+        ./getrunid.sh file.list > getfilelist.temp1
+        sed -n '1,'${1}'p' getfilelist.temp1 > getfilelist.temp2
+        echo -n "{{"
+        index=0
+        for i in `cat getfilelist.temp2`
+        do
+                echo -n "${i},$index},{"
+                index=$((index+1))
+                grep $i file.list >> file.list${1}
+        done
+        rm getfilelist.temp*
+fi
