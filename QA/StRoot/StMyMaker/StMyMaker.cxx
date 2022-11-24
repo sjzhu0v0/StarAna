2b0a627e944efe90d0cd0c30671bb60dc9ab02b2
sjzhu0v0 <zhusenjie@mail.ustc.edu.cn>
2022-11-24 15:31:44 +0800

    debug

--- QA/StRoot/StMyMaker/StMyMaker.cxx
+++ QA/StRoot/StMyMaker/StMyMaker.cxx
@@ -675,9 +675,9 @@ const Bool_t StMyMaker::isGoodEvent() const {
     return kFALSE;
   if (Vz <= StMyCuts::VzCut[0] || Vz >= StMyCuts::VzCut[1])
     return kFALSE;
-  // for (int i; i < StMyCuts::n_badrun; i++)
-  //   if (mRunIndex == StMyCuts::BadRunList[i])
-  //     return kFALSE;
+  for (int i; i < StMyCuts::NBadRun; i++)
+    if (mRunIndex == StMyCuts::BadRunList[i])
+      return kFALSE;
   // return kTRUE;
   // if(TMath::Sqrt(TMath::Power(Vx-StMyCuts::VrCen[0], 2.)+TMath::Power(Vy-StMyCuts::VrCen[1],
   // 2.))>=StMyCuts::VrCut) 	return kFALSE;
